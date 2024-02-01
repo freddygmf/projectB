80c0abab3649600e23200ce5b8fb1ef998890782
Frederico Gonçalves <111097211+freddygmf@users.noreply.github.com>
2024-02-01 15:34:11 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_mysqldb import MySQL
 from flask_sqlalchemy import SQLAlchemy
-import MySQLdb.cursors
+from sqlalchemy.sql import func
+from decimal import Decimal
 import locale
 
 # INICIALIZAÇÃO DA APLICAÇÃO
 app = Flask(__name__)
 
 # CHAVE DE SEGURANÇA DA APLICAÇÃO
-app.secret_key = 'empada'
+app.secret_key = 'B9783E9009E7B43C5A4FC70851D7023B5AF1D802290528034EE46EE1C26A4524'
 
 # CONEXÃO COM O BANCO DE DADOS
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'frederico'
-app.config['MYSQL_PASSWORD'] = 'Dequi.5812'
-app.config['MYSQL_DB'] = 'teste_carteira'
-mysql = MySQL(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+    SGBD = 'mysql+mysqlconnector',
+    usuario = 'frederico',
+    senha = 'Dequi.5812',
+    servidor = 'localhost',
+    database = 'teste_carteira'
+)
+db = SQLAlchemy(app)
+
+class Carteira1(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    nome = db.Column(db.String(90), nullable=False)
+    dia = db.Column(db.Date, nullable=False)
+    valor = db. Column(db.Float(14))
 
 # Configuração do Locale para reconhecer o REAL
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
@@ -23,59 +32,50 @@ locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 # ROTA PRINCIPAL
 @app.route('/')
 def index():
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
-    cursor.execute('SELECT * FROM teste_carteira.carteira1') 
-    dados_carteira = cursor.fetchall()
-    cursor.close()
+    dados_carteira = Carteira1.query.order_by(Carteira1.id).all()
+
+    # Calculando a soma dos valores
+    total_valores = db.session.query(func.sum(Carteira1.valor)).scalar() or 0.0
+    total_valores = locale.currency(total_valores, grouping=True)
+
     # Formatando os valores para REAL
     for item in dados_carteira:
-        item['valor'] = locale.currency(item['valor'], grouping=True)
-    return render_template('app.html', carteira=dados_carteira)
+        item.valor = locale.currency(item.valor, grouping=True)
+
+    return render_template('app.html', carteira=dados_carteira, total_valores=total_valores)
 
 # ADICIONAR NOVAS INFORMAÇÕES
-@app.route('/novo')
+@app.route('/novo', methods=['GET', 'POST'])
 def novo_item():
-    return render_template('novo.html')
-
-@app.route('/criar', methods=['GET', 'POST'])
-def criar():
     if request.method == 'POST':
         nome = request.form.get('nome')
         dia = request.form.get('dia')
         valor = request.form.get('valor')
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor) 
-        cursor.execute('INSERT INTO carteira1 VALUES (NULL, % s, % s, % s)', (nome, dia, valor, )) 
-        mysql.connection.commit()
-    return redirect(url_for('index'))
+        novo_valor = Carteira1(nome=nome, dia=dia, valor=valor)
+        db.session.add(novo_valor)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('novo.html')
 
 # EDITAR INFORMAÇÕES SALVAS
 @app.route('/edit/<int:id>', methods=["GET", 'POST'])
-def modificar_item(id):
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM carteira1 WHERE id=%s', (id,))
-    dados_item = cursor.fetchone()
-    cursor.close()
-    return render_template('editar.html', item=dados_item)
-
-@app.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editar_item(id):
+    item_para_editar = Carteira1.query.get(id)
     if request.method == 'POST':
-        nome = request.form.get('nome')
-        dia = request.form.get('dia')
-        valor = request.form.get('valor')
-        cursor = mysql.connection.cursor()
-        cursor.execute('UPDATE carteira1 SET nome=%s, dia=%s, valor=%s WHERE id=%s', (nome, dia, valor, id))
-        mysql.connection.commit()
-        cursor.close()
-    return redirect('/')
+        item_para_editar.nome = request.form.get('nome')
+        item_para_editar.dia = request.form.get('dia')
+        item_para_editar.valor = request.form.get('valor')
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editar.html', item=item_para_editar)
 
 # APAGAR INFORMAÇÕES SALVAS
 @app.route('/excluir/<int:id>', methods=['POST', ])
 def excluir_item(id):
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('DELETE FROM carteira1 WHERE id=%s', (id,))
-    mysql.connection.commit()
-    cursor.close()
+    item_remover = Carteira1.query.get(id)
+    if item_remover:
+        db.session.delete(item_remover)
+        db.session.commit()
     return redirect(url_for('index'))
 
 # START DA APLICAÇÃO
